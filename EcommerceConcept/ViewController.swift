60f525f42811ce3f6a38673548a0a039d12f5392
KodAndN <andrkokaz@gmail.com>
2022-12-22 19:24:50 +0300

    first commit

---
+++ EcommerceConcept/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcommerceConcept
+//
+//  Created by Андрей Кодочигов on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
